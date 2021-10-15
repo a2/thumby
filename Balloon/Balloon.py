e0f9217d67e7caa2302ab1d2be96bffcc84f613b
Alexsander Akers <me@a2.io>
2021-10-14 20:18:04 -0400

    Initial commit

---
+++ Balloon/Balloon.py
@@ -0,0 +1,119 @@
+# Balloon
+# by a2
+
+# https://tinycircuits.github.io/
+
+import machine
+import math
+import thumby
+import time
+
+W = thumby.DISPLAY_W
+H = thumby.DISPLAY_H
+
+balloonCache = []
+shineCache = []
+def drawBalloon(x0, y0, radius, color = 1):
+    global balloonCache
+    global shineCache
+
+    if not balloonCache:
+        for degrees in range(-90, 91, 10): # 91 to go "through" 90
+            theta = math.radians(degrees)
+            r = (1 + 0.375 / math.cosh(2.75 * (theta + math.pi /2))) / 1.375
+            x = r * math.cos(theta)
+            y = r * math.sin(theta)
+            balloonCache.append((x, y))
+            
+            if degrees >= 20 and degrees < 60:
+                shineCache.append((0.5 * math.cos(theta), 0.5 * math.sin(theta)))
+
+    # shine is distinct from the border
+    if radius >= 7:
+        shine = [(round(x * radius), round(y * radius)) for x, y in shineCache]
+        for i in range(len(shine) - 1):
+            (x1, y1) = shine[i]
+            (x2, y2) = shine[i + 1]
+            thumby.display.drawLine(x0+x1, y0-y1, x0+x2, y0-y2, color)
+
+    balloon = [(round(x * radius), round(y * radius)) for x, y in balloonCache]
+    for i in range(len(balloon) - 1):
+        (x1, y1) = balloon[i]
+        (x2, y2) = balloon[i + 1]
+        thumby.display.drawLine(x0-x1, y0-y1, x0-x2, y0-y2, color)
+        thumby.display.drawLine(x0+x1, y0-y1, x0+x2, y0-y2, color)
+
+def main():
+    time.sleep_ms(1) # Why does this fix everything?
+    thumby.display.fill(0)
+    thumby.display.drawText("Balloon", 8, 0, 1)
+    thumby.display.drawText("Press A/B", 0, 32, 1)
+    thumby.display.update()
+
+    # Wait for the user to start
+    while thumby.actionPressed() == True:
+        pass
+    while thumby.actionPressed() == False:
+        pass
+    
+    size = 1
+    t0 = time.ticks_ms()
+    d0 = 5 * 1000
+    waiting = "A"
+    
+    # Repeat for 5 seconds
+    while time.ticks_diff(time.ticks_ms(), t0) < d0:
+        thumby.display.fill(0)
+        thumby.display.drawLine(0, H-1, W - int(W * time.ticks_diff(time.ticks_ms(), t0) / d0), H-1, 1)
+        thumby.display.drawText(str(size), 0, H-10, 1)
+        drawBalloon(W//2-1, H-5-size, size, 1)
+        thumby.display.update()
+    
+        if waiting == "A":
+            if thumby.buttonA.pressed() and not thumby.buttonB.pressed():
+                waiting = "B"
+        elif waiting == "B":
+            if thumby.buttonB.pressed() and not thumby.buttonA.pressed():
+                waiting = None
+        else:
+            size += 1
+            waiting = "A"
+    
+    t1 = time.ticks_ms()
+    d1 = 7 * 1000
+    
+    dx = 0
+    dy = 0
+    
+    # Repeat for 7 seconds
+    while time.ticks_diff(time.ticks_ms(), t1) < d1:
+        thumby.display.fill(0)
+        drawBalloon(W//2-1+int(dx), H-5-size+dy, size, 1)
+        thumby.display.drawLine(W//2-1+int(dx), H-5-size+dy+size, W//2-1+int(dx), H+10+dy, 1)
+    
+        x = int(W/2)-4
+        if size >= 10:
+            x -= 4
+        thumby.display.drawText(str(size), x, max(H+10+4+dy, H//2-4), 1)
+        thumby.display.update()
+    
+        dx += math.cos(0.1 * math.pi * float(time.ticks_diff(time.ticks_ms(), t1)))
+        dy -= 1
+        time.sleep_ms(100)
+    
+    # Ask to play again
+    thumby.display.fill(0)
+    thumby.display.drawText("Again?", 12, 0, 1)
+    thumby.display.drawText("B:N A:Y", 8, 32, 1)
+    thumby.display.update()
+    
+    while not thumby.actionPressed():
+        pass
+    
+    if thumby.buttonB.pressed():
+        while thumby.buttonB.pressed():
+            pass
+        machine.reset()
+
+while True:
+    main()
---
+++ Stacker/Stacker.py
@@ -0,0 +1,177 @@
+import machine
+import math
+import random
+import thumby
+import time
+
+class FallingBlock:
+    def __init__(self, column, row, endRow):
+        self.column = column
+        self.row = row
+        self.endRow = endRow
+
+    @property
+    def landed(self):
+        return self.row <= self.endRow
+
+class Game:
+    def __init__(self):
+        self.board = [[0] * 11] * 15
+        self.animTick = 0
+        self.animFrame = 0
+        self.animDir = 1
+        self.animGameOver = False
+        self.playerLives = 3
+        self.playerLevel = 0
+        self.fallingBlocks = []
+        self.fallingFinishDelay = None
+
+    @property
+    def running(self):
+        if self.fallingBlocks:
+            return True
+        return self.playerLevel < 15 and self.playerLives > 0
+
+    def __updateAction(self):
+        for col in range(11):
+            if self.board[self.playerLevel][col] and (col < 2 or col > 8 or (self.playerLevel > 0 and not self.board[self.playerLevel - 1][col])):
+                # Add falling block if column is in visible range (2 to 8)
+                if col >= 2 and col <= 8:
+                    endRow = 0
+                    while self.board[endRow][col]:
+                        endRow += 1
+                    self.fallingBlocks.append(FallingBlock(col, self.playerLevel, endRow))
+
+                self.board[self.playerLevel][col] = 0
+                self.playerLives -= 1
+
+        if self.playerLives <= 0:
+            # Game Over
+            for fallingBlock in self.fallingBlocks:
+                fallingBlock.endRow = fallingBlock.row
+            
+            self.animGameOver = True
+
+            for row in range(self.playerLevel, -1, -1):
+                for col in range(2, 9):
+                    if self.board[row][col]:
+                        self.fallingBlocks.append(FallingBlock(col, row, -1))
+                        self.board[row][col] = 0
+        
+        self.playerLevel += 1
+        self.animFrame = random.randrange(8)
+        self.animDir = random.randrange(-1, 1, 2)
+
+        if self.playerLevel == 15:
+            # Win Grand Prize?
+            pass
+
+        # Reduce player's lives to increase difficulty
+        if self.playerLevel > 2 and self.playerLevel <= 5:
+            if self.playerLives > 2:
+                self.playerLives = 2
+        elif self.playerLevel > 8 and self.playerLevel <= 11:
+            if self.playerLives > 1:
+                self.playerLives = 1
+
+    def __updateAnimation(self):
+        move = False
+        
+        self.animTick += 1
+        if self.playerLevel < 3:
+            move = self.animTick > 7
+        elif self.playerLevel < 6:
+            move = self.animTick > 6
+        elif self.playerLevel < 9:
+            move = self.animTick > 5
+        elif self.playerLevel < 12:
+            move = self.animTick > 4
+        else:
+            move = self.animTick > 3
+        
+        if move and self.playerLevel < 15:
+            self.animTick = 0
+            
+            if self.animDir > 0:
+                self.animFrame += 1
+                if self.animFrame >= 8:
+                    self.animDir = -1
+            else:
+                self.animFrame -= 1
+                if self.animFrame <= 3 - self.playerLives:
+                    self.animDir = 1
+
+            row = [0] * 11
+            if not self.fallingBlocks:
+                row[self.animFrame:(self.animFrame + self.playerLives)] = [1] * self.playerLives
+            self.board[self.playerLevel] = row
+        
+        if self.fallingBlocks:
+            if self.animGameOver and self.fallingBlocks:
+                fallingBlock = self.fallingBlocks[-1]
+                if fallingBlock.row > fallingBlock.endRow:
+                    fallingBlock.row -= 1
+                else:
+                    del self.fallingBlocks[-1]
+            else:
+                for fallingBlock in self.fallingBlocks:
+                    if fallingBlock.row > fallingBlock.endRow:
+                        fallingBlock.row -= 1
+
+            # Have all blocks landed?
+            if all(fallingBlock.landed for fallingBlock in self.fallingBlocks):
+                if self.fallingFinishDelay is None:
+                    self.fallingFinishDelay = 28
+                elif self.fallingFinishDelay > 0:
+                    self.fallingFinishDelay -= 1
+                else:
+                    self.fallingFinishDelay = None
+                    self.fallingBlocks = []
+
+    def update(self):
+        self.__updateAnimation()
+
+        if not self.fallingBlocks and thumby.actionJustPressed():
+            self.__updateAction()
+    
+    def __drawBlock(self, column, row):
+        thumby.display.fillRect(29 + 2 * column, thumby.DISPLAY_H - 7 - 2 * row, 2, 2) 
+        
+    
+    def drawOnce(self):
+        thumby.display.fill(0)
+        
+        # Draws checkerboard background
+        # for x in range(thumby.DISPLAY_W):
+        #     for y in range(thumby.DISPLAY_H):
+        #         if (x ^ y) & 1 == 0:
+        #             thumby.display.setPixel(x, y, 1)
+        
+        thumby.display.fillRect(25, 1, 22, 38, 1)
+
+    def draw(self):
+        thumby.display.fillRect(27, 3, 18, 34, 0)
+
+        for row, array in enumerate(self.board):
+            for col, bit in enumerate(array[2:-2]):
+                if bit:
+                    self.__drawBlock(col, row)
+
+        if self.animGameOver or self.fallingFinishDelay is None or self.fallingFinishDelay % 8 >= 4:
+            for fallingBlock in self.fallingBlocks:
+                if fallingBlock.row >= 0: # Game over falls have `endRow == -1`
+                    # Subtract 2 from column because column includes the 0,1 hidden columns
+                    self.__drawBlock(fallingBlock.column - 2, fallingBlock.row) 
+
+        thumby.display.update()
+
+while True:
+    game = Game()
+    game.drawOnce()
+
+    while game.running:
+        start = time.ticks_ms()
+        game.update()
+        duration = time.ticks_diff(time.ticks_ms(), start)
+        time.sleep_ms(1000//60 - duration)
+        game.draw()
